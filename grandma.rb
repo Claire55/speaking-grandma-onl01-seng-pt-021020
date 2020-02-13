15d8d9315bbdc9fae439168a909be42e11b1c24c
Claire Bullock <bullockcm13@gmail.com>
2020-02-13 20:22:28 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,6 +2,7 @@
     
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
+
 # unless you shout it (type in all capitals).
   
 # If you shout, she can hear you (or at least she thinks so) 
